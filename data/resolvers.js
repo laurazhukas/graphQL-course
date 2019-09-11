5e175128a855cb9177c755998a2a58f26a185d0a
laurazhukas <lzhukas@hotmail.ca>
2019-09-11 09:26:21 -0400

    updating all files

---
+++ data/resolvers.js
@@ -0,0 +1,36 @@
+import mongoose from 'mongoose';
+import {Friends} from './dbConnectors';
+import { resolve } from 'url';
+import { rejects } from 'assert';
+
+// resolver map
+export const resolvers = {
+    Query:{
+        getFriend: ({ id }) => {
+            return new Friend(id, friendDatabase[id]);
+        },
+    },
+    Mutation: {
+        createFriend: (root, {input}) => {
+            const newFriend = new Friends({
+                firstName: input.firstName,
+                lastName: input.lastName,
+                gender: input.gender,
+                age: input.age,
+                language: input.language,
+                email: input.email,
+                contacts: input.contacts,
+            });
+
+            newFriend.id = newFriend.id;
+
+            return new Promise((resolve, object) => {
+                newFriend.save((err) => {
+                    if (err) reject(err)
+                    else resolve(newFriend)
+                })
+            })
+        },
+    },
+    
+};
