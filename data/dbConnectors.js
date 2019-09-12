b346de00a0876aa318189b193650e179bd3b72ca
laurazhukas <lzhukas@hotmail.ca>
2019-09-12 15:22:00 -0400

    added data persistence w SQL setup MongoDB/GraphQL

--- data/dbConnectors.js
+++ data/dbConnectors.js
@@ -1,9 +1,12 @@
 import mongoose from 'mongoose';
+import Sequelize from 'sequelize';
+import _ from 'lodash';
+import casual from 'casual';
 
 //Mongo connection
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/friends', {
-    useMongoClient: true
+    // useMongoClient: true
 });
 
 const friendSchema = new mongoose.Schema ({
@@ -32,4 +35,26 @@ const friendSchema = new mongoose.Schema ({
 
 const Friends = mongoose.model('friends', friendSchema);
 
-export {Friends};
+// SQL
+const sequelize = new Sequelize('database', null, null, {
+    dialect: 'sqlite',
+    storage: './aliens.sqlite',
+});
+
+const Aliens = sequelize.define('aliens', {
+    firstName: { type: Sequelize.STRING },
+    lastName: { type: Sequelize.STRING },
+    planet: { type: Sequelize.STRING },
+});
+
+Aliens.sync({force: true}).then(() => {
+    _.times(10, (i) => {
+        Aliens.create({
+            firstName: casual.first_name,
+            lastName: casual.last_name,
+            planet: casual.word,
+        });
+    });
+});
+
+export {Friends , Aliens };
